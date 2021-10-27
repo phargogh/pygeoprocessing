6a0644de98fee786c447a77bd1734a1b37444e63
James Douglass <jamesdouglassusa@gmail.com>
2021-10-27 16:57:16 -0700

    Adding 3.9, 3.10 and also fixing yml indentation.

--- setup.py
+++ setup.py
@@ -57,6 +57,8 @@ setup(
         'Operating System :: POSIX',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Topic :: Scientific/Engineering :: GIS',
         'License :: OSI Approved :: BSD License'
     ],
